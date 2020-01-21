8ffd61f2f7071d02e27e6a6cd79f30edb2cc3786
aadi350 <a.aadi380@gmail.com>
2020-01-21 17:03:37 -0400

    Quicksort points in ascending order by Y-value

--- android/app/src/main/java/com/helloarbridge4/SizeCheck/ObjectDetection.java
+++ android/app/src/main/java/com/helloarbridge4/SizeCheck/ObjectDetection.java
@@ -3,9 +3,10 @@ package com.helloarbridge4.SizeCheck;
 import com.google.ar.core.PointCloud;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.ux.TransformableNode;
-import com.helloarbridge4.Object.SceneFormObject;
 
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObjectDetection {
     private final float OBJ_THRESH = 0.5f;
@@ -13,6 +14,8 @@ public class ObjectDetection {
     private static final Vector3 REGION_LIMITS = new Vector3(0.8f,0.8f,1.0f);
     private static ObjectDetection objectDetection = new ObjectDetection();
     private PointCloud pointCloud;
+    FloatBuffer points;
+    private List<Point> pointList = new ArrayList<Point>();
 
     public static ObjectDetection getObjectDetector() {
         return objectDetection;
@@ -25,17 +28,13 @@ public class ObjectDetection {
 
     public void loadPointCloud(PointCloud pointCloud) {
         this.pointCloud = pointCloud;
+        this.points = pointCloud.getPoints();
     }
 
-    public boolean objectWithinRegion(TransformableNode node) {
-        if (pointCloud== null || node == null) {
-            return false;
+    public void loadValidPoints() throws NullPointerException{
+        if (pointCloud== null) {
+            throw new NullPointerException("PointCloud null");
         }
-        FloatBuffer points = pointCloud.getPoints();
-
-        float pointsInRegion = 0f;
-        float pointsTotal = 0f;
-
 
         while (points.hasRemaining()) {
             Point point = new Point(
@@ -45,16 +44,89 @@ public class ObjectDetection {
             );
 
             if (point.isValid(points.get())) {
-                pointsTotal++;
-                if (point.isInRegion(REGION_LIMITS, node)) {
-                    pointsInRegion++;
-                }
+                pointList.add(point);
             }
         }
-        System.out.println("In Region: " + pointsInRegion);
-        System.out.println("Out of region: " + pointsTotal);
-
-        return (pointsInRegion / pointsTotal) > OBJ_THRESH;
     }
+
+    public boolean isObjectDetected(TransformableNode node) {
+        if (node == null) {
+            return false;
+        }
+
+        float pointsInRegion = 0.0f;
+        float pointsTotal = 0.0f;
+        Vector3 nodeLocation = node.getWorldPosition();
+        while (points.hasRemaining()) {
+            Point point  = new Point(
+                    points.get(),
+                    points.get(),
+                    points.get()
+            );
+            boolean isPointValid = point.isValid(points.get());
+
+            if (isPointValid) {
+                if (
+                        (point.getXYZ().x - nodeLocation.x < REGION_LIMITS.x) &&
+                                (point.getXYZ().y - nodeLocation.x < REGION_LIMITS.y) &&
+                                (point.getXYZ().z - nodeLocation.z < REGION_LIMITS.z)
+                ) {
+                    pointsInRegion++;
+                }
+                pointsTotal++;
+                pointList.add(point);
+            }
+        }
+        return (pointsInRegion/pointsTotal) > OBJ_THRESH;
+    }
+
+    public void generateHull() {
+        List<Vector3> cvHull = new ArrayList<Vector3>();
+    }
+
+    private List<Point> getLowestY() {
+        List<Point> sortedList = new ArrayList<Point>();
+
+
+
+        return sortedList;
+    }
+
+    private void sortAscendingY(Point[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        sortAscending(arr,low, high);
+    }
+
+    private int getPartition(Point[] arr, int low, int high) {
+        Point pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (arr[j].getY() < pivot.getY()) {
+                i++;
+
+                //Swap
+                Point temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        Point temp = arr[i+1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return (i + 1);
+    }
+
+    private void sortAscending(Point[] arr, int low, int high) {
+        if (low < high) {
+            int pi = getPartition(arr, low, high);
+
+            sortAscending(arr, low, pi - 1);
+            sortAscending(arr, pi + 1, high);
+        }
+    }
+
 
 }
