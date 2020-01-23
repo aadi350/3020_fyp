3335d4c1c4fef7efb406d70e1f831ac74a7644a4
aadi350 <a.aadi380@gmail.com>
2020-01-23 00:38:16 -0400

    Points filtered by region implemented

--- android/app/src/main/java/com/helloarbridge4/SizeCheck/Point.java
+++ android/app/src/main/java/com/helloarbridge4/SizeCheck/Point.java
@@ -1,16 +1,57 @@
 package com.helloarbridge4.SizeCheck;
 
 import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class Point {
     private static final Float POINT_THRESH = 0.7f;
-    Vector3 pointLocation;
+    private static final float THRESH_HORIZONTAL = 0.8f;
+    private static final float THRESH_VERTICAL = 0.8f;
+    private Vector3 pointLocation;
 
     Point (Float x, Float y, Float z) {
         this.pointLocation = new Vector3(x,y,z);
     }
 
-    boolean isValid(Float confidence) {
+    public boolean isValid(Float confidence) {
         return (confidence > POINT_THRESH);
     }
+
+    public Vector3 getXYZ() {
+        return pointLocation;
+    }
+
+    public float getX() {
+        return pointLocation.x;
+    }
+
+    public float getY() {
+        return pointLocation.y;
+    }
+
+    public float getZ() {
+        return pointLocation.z;
+    }
+
+    public static boolean filterByDistanceTo(TransformableNode node, Point point) {
+        if (node == null || point == null) {
+            return false;
+        }
+        Vector3 nodeCentre = node.getWorldPosition();
+        Vector3 pointLocation = point.getXYZ();
+        Vector3 distanceToCentre = Vector3.subtract(pointLocation,nodeCentre);
+        return pointWithinThreshold(distanceToCentre);
+    }
+
+    private static boolean pointWithinThreshold(Vector3 dist) {
+        if (dist == null) return false;
+
+        return (
+                dist.x <= THRESH_HORIZONTAL &&
+                        dist.y <= THRESH_HORIZONTAL &&
+                        dist.z <= THRESH_VERTICAL
+                );
+    }
+
+
 }
