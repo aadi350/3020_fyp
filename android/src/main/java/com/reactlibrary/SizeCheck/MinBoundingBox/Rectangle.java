92c2030028ebe8525a7783ccf777c8a269b2504c
aadi350 <a.aadi380@gmail.com>
2020-05-03 23:48:03 -0400

    Perimeter removed

--- android/src/main/java/com/reactlibrary/SizeCheck/MinBoundingBox/Rectangle.java
+++ android/src/main/java/com/reactlibrary/SizeCheck/MinBoundingBox/Rectangle.java
@@ -19,9 +19,6 @@ public class Rectangle extends Polygon {
         calcCenter();
     }
 
-    public double perimeter()  {
-        return 2*(width +  height);
-    }
 
     public double area()
     {
